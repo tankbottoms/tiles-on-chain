956de40e2a7c893a51bafccd5d4179f21834aea4
tankbottoms <tanktops@tankbottoms.xyz>
2022-08-02 19:27:59 -0400

    Updated Infinite Tiles v1.2 metadata

--- scripts/mainnet.ts
+++ scripts/mainnet.ts
@@ -2,88 +2,98 @@ import * as hre from 'hardhat';
 import { ethers } from 'hardhat';
 
 enum PriceFunction {
-    LINEAR,
-    EXP,
+  LINEAR,
+  EXP,
 }
 
 const jbDirectory: any = {
-    mainnet: '0xCc8f7a89d89c2AB3559f484E0C656423E979ac9C',
-    rinkeby: '0x1A9b04A9617ba5C9b7EBfF9668C30F41db6fC21a',
+  mainnet: '0xCc8f7a89d89c2AB3559f484E0C656423E979ac9C',
+  rinkeby: '0x1A9b04A9617ba5C9b7EBfF9668C30F41db6fC21a',
+};
+
+const tilesMetadata = {
+  name: `Infinite Tiles v1.2`,
+  symbol: `TILESv1.2`,
+  openSeaMetadata: `ipfs://QmNkxcqmAi1bWvNPguPwdjc4Ka8PW6JAw6Rg7x4u5iZvKG`,
 };
 
 async function main() {
-    const projectId = '41';
-    const basePrice = ethers.utils.parseEther('0.0001');
-    const priceCap = ethers.utils.parseEther('128');
-    const multiplier = 2;
-    const tierSize = 512;
-    const jbDirectoryAddress = jbDirectory[hre.network.name];
+  const projectId = '41';
+  const basePrice = ethers.utils.parseEther('0.0001');
+  const priceCap = ethers.utils.parseEther('128');
+  const multiplier = 2;
+  const tierSize = 512;
+  const jbDirectoryAddress = jbDirectory[hre.network.name];
 
-    const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
 
-    const stringHelpersFactory = await ethers.getContractFactory('StringHelpers', deployer);
-    const stringHelpersLibrary = await stringHelpersFactory.connect(deployer).deploy();
+  const stringHelpersFactory = await ethers.getContractFactory('StringHelpers', deployer);
+  const stringHelpersLibrary = await stringHelpersFactory.connect(deployer).deploy();
 
-    const tileContentProviderFactory = await ethers.getContractFactory('TileContentProvider', {
-        libraries: { StringHelpers: stringHelpersLibrary.address },
-        signer: deployer,
+  const tileContentProviderFactory = await ethers.getContractFactory('TileContentProvider', {
+    libraries: { StringHelpers: stringHelpersLibrary.address },
+    signer: deployer,
+  });
+
+  const tileContentProvider = await tileContentProviderFactory.connect(deployer).deploy();
+  await tileContentProvider.deployed();
+
+  const legacyOwnershipPriceResolverFactory = await ethers.getContractFactory(
+    'LegacyOwnershipPriceResolver',
+    deployer,
+  );
+  const legacyOwnershipPriceResolver = await legacyOwnershipPriceResolverFactory
+    .connect(deployer)
+    .deploy(
+      '0x64931F06d3266049Bf0195346973762E6996D764',
+      basePrice,
+      multiplier,
+      tierSize,
+      priceCap,
+      PriceFunction.LINEAR,
+    );
+  await legacyOwnershipPriceResolver.deployed();
+
+  const tileNFTFactory = await ethers.getContractFactory('TileNFT', deployer);
+  const tileNFT = await tileNFTFactory
+    .connect(deployer)
+    .deploy(
+      tilesMetadata.name,
+      tilesMetadata.symbol,
+      '',
+      legacyOwnershipPriceResolver.address,
+      tileContentProvider.address,
+      jbDirectoryAddress,
+      projectId,
+      tilesMetadata.openSeaMetadata,
+    );
+  await tileNFT.deployed();
+
+  console.log(`tiles: ${tileNFT.address}`); //
+
+  try {
+    await hre.run('verify:verify', {
+      address: tileNFT.address,
+      constructorArguments: [
+        tilesMetadata.name,
+        tilesMetadata.symbol,
+        '',
+        legacyOwnershipPriceResolver.address,
+        tileContentProvider.address,
+        jbDirectoryAddress,
+        projectId,
+        tilesMetadata.openSeaMetadata,
+      ],
     });
-
-    const tileContentProvider = await tileContentProviderFactory.connect(deployer).deploy();
-    await tileContentProvider.deployed();
-
-    const legacyOwnershipPriceResolverFactory = await ethers.getContractFactory('LegacyOwnershipPriceResolver', deployer);
-    const legacyOwnershipPriceResolver = await legacyOwnershipPriceResolverFactory
-        .connect(deployer)
-        .deploy(
-            '0x64931F06d3266049Bf0195346973762E6996D764',
-            basePrice,
-            multiplier,
-            tierSize,
-            priceCap,
-            PriceFunction.LINEAR);
-    await legacyOwnershipPriceResolver.deployed();
-
-    const tileNFTFactory = await ethers.getContractFactory('TileNFT', deployer);
-    const tileNFT = await tileNFTFactory
-        .connect(deployer)
-        .deploy(
-            'Tiles on Chain',
-            'TOC',
-            '',
-            legacyOwnershipPriceResolver.address,
-            tileContentProvider.address,
-            jbDirectoryAddress,
-            projectId,
-            'ipfs://metadata'
-        );
-    await tileNFT.deployed();
-
-    console.log(`tiles: ${tileNFT.address}`); // 
-
-    try {
-        await hre.run('verify:verify', {
-            address: tileNFT.address,
-            constructorArguments: [
-                'Tiles on Chain',
-                'TOC',
-                '',
-                legacyOwnershipPriceResolver.address,
-                tileContentProvider.address,
-                jbDirectoryAddress,
-                projectId,
-                'ipfs://metadata'
-            ],
-        });
-    } catch (err) {
-        console.log('could not register contract code');
-        console.log(err);
-    }
+  } catch (err) {
+    console.log('Could not register contract code');
+    console.log(err);
+  }
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
 
 // npx hardhat run scripts/rinkeby.ts --network mainnet
