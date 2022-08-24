30e708b850efa729ba6947b3b5e37e3a5158497c
cptspacecadet <m@dao-lawfirm.xyz>
2022-08-24 00:55:06 -0400

    - parameter pattern pricer

--- contracts/PatternPriceResolver.sol
+++ contracts/PatternPriceResolver.sol
@@ -50,21 +50,25 @@ import './interfaces/IPriceResolver.sol';
 error UNSUPPORTED_OPERATION();
 
 contract PatternPriceResolver is IPriceResolver {
-    uint256 basePrice;
-    uint256 multiplier;
+  uint256 basePrice;
+  uint256 multiplier;
+  uint256 matchWidth;
 
-    /**
-     * @notice blah
-     * 
-     * @param _basePrice blah
-     * @param _multiplier blah
-     */
+  /**
+   * @notice Pricing contract that prices tiles based on repeating hex characters.
+   *
+   * @param _basePrice Base NFT price
+   * @param _multiplier Pair price multiplier.
+   * @param _matchWidth Width to match against, expected to be a multiple of 4, like 16, 8, 4.
+   */
   constructor(
     uint256 _basePrice,
-    uint256 _multiplier
+    uint256 _multiplier,
+    uint256 _matchWidth
   ) {
     basePrice = _basePrice;
     multiplier = _multiplier;
+    matchWidth = _matchWidth;
   }
 
   /**
@@ -101,40 +105,48 @@ contract PatternPriceResolver is IPriceResolver {
   ) public view virtual override returns (uint256 price) {
     uint160 tileAddress = uint160(bytesToAddress(_params, 32));
 
-    uint16[9] memory sections;
-    for (uint8 i; i != 9;) {
-        sections[i] = uint16(tileAddress >> ((8 - i) * 16));
-        ++i;
+    /**
+     * @dev 36 is the max number of 4-bit sections we can have in the 144bit stretch representing the image portions.
+     */
+
+    // uint8 sectionIncrement = uint8(36 / (matchWidth / 4));
+    uint8 sectionIncrement = uint8(matchWidth / 4);
+
+    uint16[36] memory sections;
+    for (uint8 i; i != 36; ) {
+      uint16 shift = uint16(160 - 16 - matchWidth * (1 + i / sectionIncrement));
+      sections[i] = uint16((tileAddress >> shift) & (2**matchWidth - 1));
+      i += sectionIncrement;
     }
 
-    uint8[9] memory matches;
-    bool[9] memory matched;
-    for (uint8 i; i != 9;) {
-        if (matched[i]) {
-            ++i;
-            continue;
+    uint8[36] memory matches;
+    bool[36] memory matched;
+    for (uint8 i; i != 36; ) {
+      if (matched[i]) {
+        i += sectionIncrement;
+        continue;
+      }
+
+      for (uint8 j; j != 36; ) {
+        if (i == j || matched[j]) {
+          j += sectionIncrement;
+          continue;
         }
 
-        for (uint8 j; j != 9;) {
-            if (i == j || matched[j]) {
-                ++j;
-                continue;
-            }
-
-            if (sections[i] == sections[j]) {
-                matched[j] = true;
-                ++matches[i];
-            }
-
-            ++j;
+        if (sections[i] == sections[j]) {
+          matched[j] = true;
+          ++matches[i];
         }
-        ++i;
+
+        j += sectionIncrement;
+      }
+      i += sectionIncrement;
     }
 
     uint8 matchCount;
-    for (uint8 i; i != 9;) {
-        matchCount += matches[i];
-        ++i;
+    for (uint8 i; i != 36; ) {
+      matchCount += matches[i];
+      i += sectionIncrement;
     }
 
     price = basePrice + matchCount * multiplier;
@@ -143,8 +155,9 @@ contract PatternPriceResolver is IPriceResolver {
   function bytesToAddress(bytes memory b, uint256 offset) private pure returns (address account) {
     bytes32 raw;
 
-    for (uint256 i = 0; i < 20; i++) {
+    for (uint256 i = 0; i != 20; ) {
       raw |= bytes32(b[offset + i] & 0xFF) >> (i * 8);
+      ++i;
     }
 
     account = address(uint160(uint256(raw >> 96)));
