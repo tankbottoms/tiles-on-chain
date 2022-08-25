7826be2421c3c2cfdc26a3f4a8b0d13afda97efc
cptspacecadet <m@dao-lawfirm.xyz>
2022-08-25 13:49:17 -0400

    - OG script updates

--- scripts/og-mint.ts
+++ scripts/og-mint.ts
@@ -37,6 +37,16 @@ async function getAbiForAddress(contractAddress: string) {
     return abi;
 }
 
+async function getGasPrice() {
+    const etherscanKey = process.env.ETHERSCAN_API_KEY || '';
+
+    const result = await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${etherscanKey}`)
+        .then((response) => response.json())
+        .then((data) => data['result']);
+
+    return result;
+}
+
 function sleep(ms = 1_000) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -59,6 +69,8 @@ async function main() {
 
     const oldTileCount = (await oldTileNFT.connect(deployer).totalSupply()).toNumber();
 
+    logger.info(`connected as ${deployer.address}`);
+    let mintPairs: any[] = [];
     for (let i = 1; i <= oldTileCount; i++) {
         const tileAddress = await oldTileNFT.connect(deployer).tileAddressOf(i);
         const oldOwner = await oldTileNFT.connect(deployer).ownerOf(i);
@@ -67,19 +79,7 @@ async function main() {
         const newTileIndex = (await newTileNFT.connect(deployer).idForAddress(tileAddress)).toNumber();
 
         if (newTileIndex === 0) {
-            logger.info(`minting ${tileAddress} to OG ${oldOwner}`);
-            let tries = 0;
-            while (tries < 10) {
-                try {
-                    const tx = await newTileNFT.connect(deployer).superMint(oldOwner, tileAddress, { gasPrice: 7_000_000_000, gasLimit: 300_000 });
-                    const receipt = await tx.wait();
-                    tries = 11;
-                } catch (err) {
-                    logger.info(err);
-                    await sleep(10_000);
-                    tries++;
-                }
-            }
+            mintPairs.push({ owner: oldOwner, tile: tileAddress });
         } else {
             const newOwner = await newTileNFT.connect(deployer).ownerOf(newTileIndex);
             if (newOwner == oldOwner) {
@@ -90,6 +90,36 @@ async function main() {
         }
     }
 
+    logger.info(`found ${mintPairs.length} mintable tiles`);
+
+    const startTime = Date.now();
+    let checkpoint = Date.now();
+    let minted = 0;
+    let failed = 0;
+    for await (const pair of mintPairs) {
+        let gasSummary = await getGasPrice();
+        logger.info(`gas ${gasSummary['SafeGasPrice']}`)
+        while (Number((await getGasPrice())['SafeGasPrice']) !== 2) {
+            await sleep(30_000);
+
+            if (Date.now() - checkpoint > 30 * 60 * 1000) {
+                logger.info(`ran for ${(Date.now() - startTime) / (60 * 1000)}min, minted ${minted}, failed ${failed}`);
+                checkpoint = Date.now();
+            }
+        }
+
+        logger.info(`minting ${pair.tile} to OG ${pair.owner}`);
+        try {
+            const tx = await newTileNFT.connect(deployer).superMint(pair.owner, pair.tile, { gasPrice: 2_000_000_000, gasLimit: 300_000 });
+            const receipt = await tx.wait();
+            minted++;
+            logger.info(`minted ${pair.tile} to OG ${pair.owner}}`);
+        } catch (err) {
+            logger.error(`failed to mint ${pair.tile} to OG ${pair.owner}`);
+            logger.error(err);
+            failed++;
+        }
+    }    
 }
 
 main().catch((error) => {
