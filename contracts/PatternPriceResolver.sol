733dae6d1f614f45fab23e6c13a0951603b44573
cptspacecadet <m@dao-lawfirm.xyz>
2022-09-05 14:30:13 -0400

    - remove legacy grant from pattern resolver

--- contracts/PatternPriceResolver.sol
+++ contracts/PatternPriceResolver.sol
@@ -51,7 +51,7 @@ import './interfaces/IPriceResolver.sol';
 error UNSUPPORTED_OPERATION();
 
 contract PatternPriceResolver is IPriceResolver {
-  IOriginalInfiniteTiles private immutable legacyContract;
+//   IOriginalInfiniteTiles private immutable legacyContract;
   uint256 basePrice;
   uint256 multiplier;
   uint256 matchWidth;
@@ -62,18 +62,17 @@ contract PatternPriceResolver is IPriceResolver {
    * @param _basePrice Base NFT price
    * @param _multiplier Pair price multiplier.
    * @param _matchWidth Width to match against, expected to be a multiple of 4, like 16, 8, 4.
-   * @param _legacyContract Original Tiles NFT contract
    */
   constructor(
     uint256 _basePrice,
     uint256 _multiplier,
-    uint256 _matchWidth,
-    IOriginalInfiniteTiles _legacyContract
+    uint256 _matchWidth
+    // IOriginalInfiniteTiles _legacyContract
   ) {
     basePrice = _basePrice;
     multiplier = _multiplier;
     matchWidth = _matchWidth;
-    legacyContract = _legacyContract;
+    // legacyContract = _legacyContract;
   }
 
   /**
@@ -100,25 +99,25 @@ contract PatternPriceResolver is IPriceResolver {
   /**
    * @notice Returns the price for a tile based on pattern repetition unless the tile being minted is owned on the original contract by the minter, in which case the price will be 0.
    *
-   * @param _account Minter address.
+   * @param (address) Minter address.
    * @param (uint256) Next token id is ignored.
    * @param _params Item at index 0 is expected to be an encoded uint256 representing current supply, followed by a 20-byte tile address.
    */
   function getPriceWithParams(
-    address _account,
+    address,
     uint256,
     bytes calldata _params
   ) public view virtual override returns (uint256 price) {
     address tileAddress = bytesToAddress(_params, 32);
 
-    uint256 originalTileId = legacyContract.idOfAddress(tileAddress);
-    if (originalTileId != 0) {
-      address originalOwner = legacyContract.ownerOf(originalTileId);
+    // uint256 originalTileId = legacyContract.idOfAddress(tileAddress);
+    // if (originalTileId != 0) {
+    //   address originalOwner = legacyContract.ownerOf(originalTileId);
 
-      if (_account == originalOwner) {
-        return 0;
-      }
-    }
+    //   if (_account == originalOwner) {
+    //     return 0;
+    //   }
+    // }
 
     uint160 tileInt = uint160(tileAddress);
 
